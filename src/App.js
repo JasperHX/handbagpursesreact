6e0eb726d40d2c3d6e6e2421d7b21d19d01ec324
JasperHX <jasper-hendrikx@hotmail.com>
2021-09-12 22:53:01 +0200

    dag1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
-import Button from "./assets/components/Button";
+import Button from './assets/components/Button';
+import bag_1 from './assets/bag_1.png';
+import bag_2 from './assets/bag_2.png';
+import bag_3 from './assets/bag_3.png';
+import bag_4 from './assets/bag_4.png';
+// import Product from "./assets/components/Products";
+// import Product from './Products';
 
 function App() {
   return (
@@ -11,6 +17,40 @@ function App() {
         <button>shop all bags</button>
         <button disabled={true}>pre-orders</button>
       </nav>
+
+          <main>
+
+              {/*<Product*/}
+              {/*    image={bag_1}*/}
+              {/*    title=""*/}
+              {/*    description=""*/}
+              {/*/>*/}
+
+              <article>
+                  <span>Best Seller</span>
+                  <img src={bag_1} alt="tas_1" />
+                  <p>The Handy bag</p>
+                  <h4>&euro;400,-</h4>
+              </article>
+              <article>
+                  <span>Best Seller</span>
+                  <img src={bag_2} alt="tas_2" />
+                  <p>The stylish bag</p>
+                  <h4>&euro;250,-</h4>
+              </article>
+              <article>
+                  <span>New Collection</span>
+                  <img src={bag_3} alt="tas_3"/>
+                  <p>The simple bag</p>
+                  <h4>&euro;300,-</h4>
+              </article>
+              <article>
+                  <span>New Collection</span>
+                  <img src={bag_4} alt="tas_4"/>
+                  <p>The trendy bag</p>
+                  <h4>&euro;150,-</h4>
+              </article>
+          </main>
       </>
 
   );
---
+++ src/assets/components/Products.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Product()  {
+    return  (
+        <article className="article">
+            <span className="article.span">Best Seller</span>
+            <img src={} alt="tas_1" />
+            <p className="article.p">The Handy bag</p>
+            <h4 className="h4">&euro;400,-</h4>
+        </article>
+
+
+
+    );
+}
+
+export default Product;
